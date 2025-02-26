bd3f14985af6da37950ebd5d033e947d28569cce
yassineelcaidi <150461050+yassineelcaidi@users.noreply.github.com>
2025-02-26 11:55:27 +0000

    Update index.js to get correcltly the roomname 3

--- assets/index.js
+++ assets/index.js
@@ -1,16 +1,16 @@
 const url = new URL(location.href);
 
-// Extract the room name from the pathname
-const roomName = url.pathname.split('/').pop(); // Gets the last segment of the pathname
+// Get the room name from the URL parameters
+const params = new URLSearchParams(url.search);
+const roomName = params.get('room'); // Assuming the parameter is named 'room'
 
 const roomUrl = new URL("https://datarockstars.whereby.com");
-roomUrl.pathname = roomName; // Set the pathname to the room name
+roomUrl.pathname = roomName ? roomName : ''; // Set the pathname to the room name if it exists
 roomUrl.search = url.search;
 
 const root = document.getElementById("root");
 const whereby = document.createElement("whereby-embed");
-whereby.setAttribute("room", roomUrl.href+"/"+roomName);
-console.log(roomUrl.href+"/"+roomName)
+whereby.setAttribute("room", roomUrl.href);
 whereby.setAttribute("minimal", true);
 whereby.setAttribute("style", "height: 100%;");
 root.appendChild(whereby);
